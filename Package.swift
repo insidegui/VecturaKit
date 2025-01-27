c7dc5383f09d92a375eeccaacd806d91d2adb56a
Guilherme Rambo <insidegui@gmail.com>
2025-01-27 13:40:55 -0300

    Use swift-embeddings fork with lower deployment target

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/jkrukowski/swift-embeddings.git", branch: "main"),
+    .package(url: "git@github.com:insidegui/swift-embeddings.git", branch: "lower-deployment-target"),
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
   ],
   targets: [
